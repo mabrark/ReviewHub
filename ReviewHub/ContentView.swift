1389589249586b7566cea4dab116bdd4d687672b
mabrark <muhammad.khan@triosstudent.com>
2025-09-26 08:22:39 -0400

    Initial Commit

---
+++ ReviewHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReviewHub
+//
+//  Created by Muhammad Abrar Khan on 2025-09-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReviewHub/ReviewHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewHubApp.swift
+//  ReviewHub
+//
+//  Created by Muhammad Abrar Khan on 2025-09-26.
+//
+
+import SwiftUI
+
+@main
+struct ReviewHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
